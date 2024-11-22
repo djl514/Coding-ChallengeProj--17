07467c366baa0f152c4d5ffaf8c6d516c563debe
David Layne <davidlayne@usf.edu>
2024-11-22 11:26:43 -0500

    Implement tour fetching and rendering with state management // Style components

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -7,6 +7,7 @@ function Gallery(){
 
 
     // useEffect is used to fetch the API and JSON to allow use of the data 
+    // Catches and alets error and displays a loading message
     useEffect(() => {
         fetch('https://api.allorigins.win/get?url=https://course-api.com/react-tours-project')
           .then(response => response.json())
@@ -28,35 +29,47 @@ function Gallery(){
         //Used in the Not Interested button to remove a tour from the list
         function handleInterest(id){
             setGallery(gallery.filter(tour => tour.id !== id));
+            console.log(`Tour ${id} Removed`)
         };
 
-    // return statement for listing data, buttons for more / less interested, and a remove button
+    // return statement
     return (
-        <table>
+        <div>
             <h1>Tour Management Gallery</h1>
-            <hr></hr>
-            <dl>
+                {/* maps the galler to a table for each key ID */}
                 {gallery.map(gall => (
-                    <dt key={gall.id}>
-                        <h2>{gall.name}</h2>
-                        <dd>Price: ${gall.price}</dd>
-                        <dd> 
+                    <table key={gall.id}>
+                        {/* Grabs image */}
+                        <center><tr><div className = "polaroid"><img src={gall.image} alt= {`Tour ${gall.id}`} />
+                        
+                        <div className = "container">
+                            <h2>{gall.name}</h2>
+                        </div>
+
+                        </div></tr></center>
+                        <center>
+                            <tr><td>Price:</td>
+                            <td>${gall.price}</td></tr>
+                        </center>
+
+                        {/* Button for toggling more/less information */}
+                        <tr>
                             {information ? gall.info : `${gall.info.substring(0,250)}...`}
                             <button onClick={() => setInformation(!information)}>
                                 {information ? "Show Less" : "Read More"}
                             </button>
-                        </dd>
-                        <dd><img src={gall.image} alt= "tour" /></dd>
-                        <br></br>
-                        <button onClick={() => handleInterest(gall.id)}>Not Interested</button>
-                        <br></br>
-                    </dt>
-                ))}
-            </dl>
+                        </tr>
+                      
 
-        </table>
+                        
+                        <br></br>
+                        <tr><button onClick={() => handleInterest(gall.id)}>Not Interested</button></tr>
+                        <hr></hr>
+                    </table>
+                ))}
+        </div>
     );
 }
 
-export default Gallery;
 
+export default Gallery;
